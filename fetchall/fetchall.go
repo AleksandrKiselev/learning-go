f7f20f61aa2389b335899fad24745f71bc223c00
Aleksandr Kiselev <a.kiselev.fw@gmail.com>
2022-08-14 22:53:58 +0500

    fetchall

---
+++ fetchall/fetchall.go
@@ -0,0 +1,48 @@
+// Выполняет парралельную выборку url и сообщает время выполнения и размер ответа
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+	}
+
+	r := regexp.MustCompile(`(:\/\/|\.[a-zA-Z]+$)`)
+	filename := r.Split(url, -1)[1]
+	file, err := os.Create(filename)
+	if err != nil {
+		ch <- fmt.Sprintf("can not create file: %s, error: %v", filename, err)
+	}
+
+	nbites, err := io.Copy(file, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbites, url)
+}
