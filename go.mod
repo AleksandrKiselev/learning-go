f8e809b779b01ae3c5be1d0f7e2e4e4a0a565591
Aleksandr Kiselev <a.kiselev.fw@gmail.com>
2022-11-20 00:46:28 +0500

    refactoring

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AleksandrKiselev/learning-go
+
+go 1.18
--- src/echo-server/echo-server.go
+++ src/echo-server/echo-server.go
@@ -14,14 +14,14 @@ var count int
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		mu.Lock()
+		defer mu.Unlock()
 		count++
-		mu.Unlock()
 		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 	})
 	http.HandleFunc("/count", func(w http.ResponseWriter, _ *http.Request) {
 		mu.Lock()
+		defer mu.Unlock()
 		fmt.Fprintf(w, "Count = %d\n", count)
-		mu.Unlock()
 	})
 	log.Fatal(http.ListenAndServe("127.0.0.1:8000", nil))
 }
